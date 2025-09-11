56a3bd04f0ef800880fdad1497fa39ba81b3fbd9
Henishwiz <henish@wizcoder.com>
2025-09-11 10:39:18 +0530

    changed branch main to henish_dev

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'fbcbb5a48c41ff08dd6199f4633e24a88de2ef8e', queries,  });
+export const client = createClient({ cacheDir: 'C:/Henish Work space/welcomecure-corporate/tina/__generated__/.cache/1757567289689', url: 'https://content.tinajs.io/1.6/content/cb6ed5a0-840a-4bb0-b6e4-e7ef7658bcf2/github/henish_dev', token: 'fbcbb5a48c41ff08dd6199f4633e24a88de2ef8e', queries,  });
 export default client;
   
